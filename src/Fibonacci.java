be16a17952677054a29f13eba851f4f39dd5e7ff
Ashwaq <md.ashwaq.2609@gmail.com>
2025-11-06 11:55:12 +0530

    practice

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -5,7 +5,9 @@ public class Fibonacci {
         int first = 0;
         int second = 1;
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
         int num = sc.nextInt();
+        System.out.print("The terms are: ");
         for ( int i = 0; i <= num; i++) {
             System.out.print(first + " ");
             int next = second + first;
